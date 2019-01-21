f519cf3efd36ed199da90269a2f32b98eecb1dae
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2019-01-22 01:04:04 +0300

    Add ItemTransformer and multiple price support

--- SOLID/OrderCalculator.h
+++ SOLID/OrderCalculator.h
@@ -13,6 +13,26 @@ struct ItemInfo
 	ItemPrice total;
 };
 
+class IItemAccessor
+{
+public:
+	class IFindResult
+	{
+	public:
+		virtual ~IFindResult() = default;
+		virtual const ItemCount GetValue() const = 0;
+		virtual void SetValue(ItemCount newPrice) = 0;
+	};
+
+	virtual ~IItemAccessor() = default;
+	virtual std::unique_ptr<IFindResult> Find(ItemId const& id, ItemPrice price) = 0;
+	virtual void Insert(ItemId const& id, ItemPrice price, ItemCount count) = 0;
+	virtual void Remove(ItemId const& id, ItemPrice price) = 0;
+};
+
+using ItemTransformer = std::function<void(IItemAccessor &itemAccessor)>;
+
 using ResultAcceptor = std::function<void(ItemInfo const&)>;
 
-using OrderCalculator = std::function<void(ItemEnumerator const&, ItemPriceProvider const&, ResultAcceptor const&)>;
+using OrderCalculator = std::function<void(ItemEnumerator const&,
+	ItemPriceProvider const&, ItemTransformer const&, ResultAcceptor const&)>;
--- SOLID/Source.cpp
+++ SOLID/Source.cpp
@@ -1,16 +1,75 @@
 #include <array>
 #include <vector>
+#include <map>
 #include <iostream>
 #include "OrderCalculator.h"
 
+class ItemAccessor : public IItemAccessor
+{
+public:
+	explicit ItemAccessor(std::vector<std::map<ItemPrice, ItemCount>> &table)
+		: m_table(table)
+	{
+	}
+
+	std::unique_ptr<IFindResult> Find(ItemId const & id, ItemPrice price) final
+	{
+		const auto it = m_table[id.GetIntId()].find(price);
+		if (it == m_table[id.GetIntId()].end())
+		{
+			return std::unique_ptr<IFindResult>();
+		}
+		return std::make_unique<FoundResult>(m_table[id.GetIntId()], price);
+	}
+
+	void Insert(ItemId const & id, ItemPrice price, ItemCount count) final
+	{
+		m_table[id.GetIntId()][price] = count;
+	}
+
+	void Remove(ItemId const & id, ItemPrice price) final
+	{
+		m_table[id.GetIntId()].erase(price);
+	}
+
+private:
+	class FoundResult : public IFindResult
+	{
+	public:
+		explicit FoundResult(std::map<ItemPrice, ItemCount> &map, ItemPrice key)
+			: m_map(map)
+			, m_key(key)
+		{
+		}
+
+		const ItemCount GetValue() const final
+		{
+			return m_map[m_key];
+		}
+
+		void SetValue(ItemCount newPrice) final
+		{
+			m_map[m_key] = newPrice;
+		}
+
+	private:
+		std::map<ItemPrice, ItemCount> &m_map;
+		ItemPrice m_key;
+	};
+
+	std::vector<std::map<ItemPrice, ItemCount>> &m_table;
+};
+
 OrderCalculator GetOrderCalculator()
 {
-	return [](ItemEnumerator const& itemEnumerator, ItemPriceProvider const& priceProvider, ResultAcceptor const& resultAcceptor) {
+	return [](ItemEnumerator const& itemEnumerator, ItemPriceProvider const& priceProvider,
+			ItemTransformer const& itemTransformer, ResultAcceptor const& resultAcceptor) {
 		std::vector<ItemCount> countTable(ItemId::COUNT, 0);
 		itemEnumerator([&](ItemId const& itemId) {
 			++countTable[itemId.GetIntId()];
 		});
 
+		std::vector<std::map<ItemPrice, ItemCount>> table(ItemId::COUNT);
 		for (int itemIdInt = 0; itemIdInt < ItemId::COUNT; ++itemIdInt)
 		{
 			const auto count = countTable[itemIdInt];
@@ -21,7 +80,25 @@ OrderCalculator GetOrderCalculator()
 
 			const auto item = ItemId::CreateFromInt(itemIdInt);
 			const auto price = priceProvider(item);
-			resultAcceptor({ item, price, count, price * count });
+
+			++table[itemIdInt][price];
+		}
+
+		{
+			ItemAccessor ia(table);
+			itemTransformer(ia);
+		}
+
+		for (int itemIdInt = 0; itemIdInt < ItemId::COUNT; ++itemIdInt)
+		{
+			const auto item = ItemId::CreateFromInt(itemIdInt);
+
+			for (auto &pair : table[itemIdInt])
+			{
+				const auto price = pair.first;
+				const auto count = pair.second;
+				resultAcceptor({ item, price, count, price * count });
+			}
 		}
 	};
 }
@@ -56,12 +133,20 @@ int main()
 		return result->second;
 	};
 
+	auto itemTransformer = [](IItemAccessor &itemAccessor) {
+		auto it = itemAccessor.Find(ItemId::CreateFromChar('B'), 4);
+		it->SetValue(42);
+
+		itemAccessor.Insert(ItemId::CreateFromChar('H'), 666, 13);
+		itemAccessor.Insert(ItemId::CreateFromChar('A'), 1, 99);
+	};
+
 	auto resultPrinter = [](ItemInfo const& info) {
 		std::cout << info.id.GetCharId() << ": " << info.price << " x " << info.count << " = " << info.total << std::endl;
 	};
 
 	auto oc = GetOrderCalculator();
-	oc(itemEnumerator, itemPriceProvider, resultPrinter);
+	oc(itemEnumerator, itemPriceProvider, itemTransformer, resultPrinter);
 
 	std::cin.get();
 	return 0;
